0f4c8559390fe2804eb27f4c06fcc295eeea3ba9
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2023-09-14 16:27:31 +0200

    remove theme view

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-
 import { useMemory } from '../memory'
 import { useTranslation, supported } from '../localisation'
 import { unregister } from '../registrar'
 
 import Quickaccess from '../quickaccess'
-import Theme from '../theme'
 
 import { version } from '../meta'
-
-enum Pane { APP, LANGUAGES, THEME }
 
 export default function Options() {
 
@@ -17,60 +12,34 @@ export default function Options() {
 
     const { language, setLanguage } = useMemory()!
 
-    const [pane, setPane] = useState(Pane.APP)
-
-    const options = [
-        [Pane.APP, t`application`],
-        [Pane.THEME, t`theme`]
-    ] as [Pane, string][]
-
-    function PaneButton({ name, text }: { name: Pane, text: string}) {
-
-        return  <button data-attention='primary' data-active={pane == name}
-            onClick={() => setPane(name)}
-        >{text}</button>
-    }
-
     return <>
 
-        <Quickaccess>
-            <span className='stack'>
-                {options.map(([name, text], i) => PaneButton({ name, text }))}
-            </span>
-        </Quickaccess>
+        <Quickaccess/>
 
         <h1 className='title'>{t`options`}</h1>
 
-        {pane == Pane.APP ? <>
+        <p>{t`flisqs`} - {t`version`} {version}</p>
 
-            <h2>{t`settings`}</h2>
+        <div className='row'>
+            
+            <div>
 
-            <p>{t`flisqs`} - {t`version`} {version}</p>
-
-            <div className='row'>
-                
-                <div>
-
-                    <button style={{display:'inline-block',margin: '0 1em'}} onClick={() => 
-                        unregister().then(() => window.location.reload())
-                    }>{t`update`}</button>
-                </div>
-
-                <div>
-                    <p>Language of interface:</p>
-
-                    <select value={language} onChange={e => { setLanguage(e.target.value) }}>
-                        <option key={-1} value=''>{t`of the device`}</option>
-                        {supported.map(([code, name], i) => 
-                            <option key={i} value={code}>{name}</option>
-                        )}
-                    </select>
-                </div>
+                <button style={{display:'inline-block',margin: '0 1em'}} onClick={() => 
+                    unregister().then(() => window.location.reload())
+                }>{t`update`}</button>
             </div>
 
-        </> : null}
+            <div>
+                <p>Language of interface:</p>
 
-        {pane == Pane.THEME ? <Theme/> : null}
+                <select value={language} onChange={e => { setLanguage(e.target.value) }}>
+                    <option key={-1} value=''>{t`of the device`}</option>
+                    {supported.map(([code, name], i) => 
+                        <option key={i} value={code}>{name}</option>
+                    )}
+                </select>
+            </div>
+        </div>
 
     </>
 }
